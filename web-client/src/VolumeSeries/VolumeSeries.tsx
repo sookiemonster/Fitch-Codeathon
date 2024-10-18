60da723ca7372bee14c5c63a3551e0eb6fad9ba2
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2024-10-18 18:07:42 -0400

    Setup basic filters for time-series

--- web-client/src/VolumeSeries/VolumeSeries.tsx
+++ web-client/src/VolumeSeries/VolumeSeries.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { State } from "../StateHandler";
 
 interface SeriesProps {
@@ -6,10 +6,35 @@ interface SeriesProps {
     dispatch:any
 }
 
+enum Field { CHECKOUT = "Check-Out", DROPOFF = "Drop-off" }
+enum Interval { DAY = "Day", WEEK = "Week", MONTH = "Month", YTD = "Year-to-Date" }
+
+interface SeriesFilters {
+    item_type?:String,
+    field:Field,
+    interval:Interval
+}
+
+const initialFilters:SeriesFilters = {
+    field:Field.CHECKOUT,
+    interval: Interval.DAY
+}
+
 function VolumeSeries({state, dispatch}:SeriesProps):JSX.Element {
+    const [filter, setFilters] = useState(initialFilters);
+
+    let calcBars = () => {
+
+    }
+
+    const cachedValue = useMemo(calcBars,
+        [filter, state.inventory]
+    )
+
+    
     return (
         <div id="series-card">
-
+            <h2>{initialFilters.field} Volume Over the {initialFilters.interval} </h2>
         </div>
     )
 }
