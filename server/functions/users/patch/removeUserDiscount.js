99e5a2c6d9683bfb89ccc81cac923ac40a8aa188
TONKAFREAK <soloyjon27@gmail.com>
2024-10-18 20:58:30 -0400

    +removeUserDiscount

---
+++ server/functions/users/patch/removeUserDiscount.js
@@ -0,0 +1,35 @@
+const { prisma } = require("../../../lib/prisma");
+
+
+async function removeUserDiscount(id, discountId) {
+
+    const user = await prisma.user.findUnique({
+        where: { id: id },
+        select: {
+            discounts: true
+        }
+    });
+
+    const discount = await prisma.discount.findUnique({
+        where: { id: discountId },
+        select: {
+            id: true
+        }
+    });
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    if (!discount) {
+        throw new Error("Discount not found");
+    }
+
+    await prisma.user.update({
+        where: { id: id },
+        data: { discounts: { set: userOwner.discounts.filter((i) => i !== discountId) } },
+    });
+
+}
+
+module.exports = { removeUserDiscount }
