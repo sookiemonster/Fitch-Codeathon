4298c36a2c34cc06c4ae72143f41d65ba8d1dba8
TONKAFREAK <soloyjon27@gmail.com>
2024-10-19 11:01:08 -0400

    auth is done

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
 const registerUser = require("../../functions/authorization/user/signUp");
 const loginUser = require("../../functions/authorization/user/signIn");
+
+const vendorRegister = require("../../functions/authorization/vendor/signUp");
+const vendorLogin = require("../../functions/authorization/vendor/signIn");
+
+const washerRegister = require("../../functions/authorization/wahser/signUp");
+const washerLogin = require("../../functions/authorization/wahser/signIn");
 
 // POST /api/v1/auth for users
 
@@ -11,12 +18,14 @@ router.post("/users/login", loginUser);
 
 // POST /api/v1/auth for vendors
 
-router.post("/vendors/register", (req, res) => {
-  res.send("Making new vendor account");
-});
+router.post("/vendors/register", vendorRegister);
 
-router.post("/vendors/login", (req, res) => {
-  res.send("Logging in vendor");
-});
+router.post("/vendors/login", vendorLogin);
+
+// POST /api/v1/auth/ for washer
+
+router.post("/wahsers/register", washerRegister);
+
+router.post("/wahsers/login", washerLogin);
 
 module.exports = router;
