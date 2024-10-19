8dbbbff9e76f47a8696cd57e21d27e1b7cb3b8cf
danteb98 <danteb623@gmail.com>
2024-10-19 17:05:58 -0400

    bug fix
    -Changed condition to item status

--- server/functions/washer/patch/moveToVendor.js
+++ server/functions/washer/patch/moveToVendor.js
@@ -1,55 +1,54 @@
-const {prisma} = require("../../../lib/prisma");
+const { prisma } = require("../../../lib/prisma");
 
 async function moveToVendor(washerId, itemId, vendorId) {
+  const item = await prisma.item.findUnique({
+    where: { id: itemId },
+  });
 
-    const item = await prisma.item.findUnique({
-        where: { id: itemId },
-    });
+  if (!item) {
+    throw new Error("Item not found");
+  }
 
-    if (!item) {
-        throw new Error("Item not found");
-    }
+  if (item.status !== "Fresh") {
+    throw new Error("Item is not Clean");
+  }
 
-    if (!item.status !== "Fresh") {
-        throw new Error("Item is not Clean");
-    }
+  const washer = await prisma.washer.findUnique({
+    where: { id: washerId },
+  });
 
-    const washer = await prisma.washer.findUnique({
-        where: { id: washerId },
-    });
+  if (!washer) {
+    throw new Error("Washer not found");
+  }
 
-    if (!washer) {
-        throw new Error("Washer not found");
-    }
+  const vendor = await prisma.vendor.findUnique({
+    where: { id: vendorId },
+  });
 
-    const vendor = await prisma.vendor.findUnique({
-        where: { id: vendorId },
-    });
+  if (!vendor) {
+    throw new Error("Vendor not found");
+  }
 
-    if (!vendor) {
-        throw new Error("Vendor not found");
-    }
+  if (item.owner !== washerId) {
+    throw new Error("This item is not owned by this washer");
+  }
 
-    if (item.owner !== washerId) {
-        throw new Error("This item is not owned by this washer");
-    }
+  await prisma.vendor.update({
+    where: { id: vendorId },
+    data: { inventory: { push: itemId } },
+  });
 
-    await prisma.vendor.update({
-        where: { id: vendorId },
-        data: { inventory: { push: itemId } },
-    });
+  await prisma.washer.update({
+    where: { id: washerId },
+    data: { inventory: { set: washer.inventory.filter((i) => i !== itemId) } },
+  });
 
-    await prisma.washer.update({
-        where: { id: washerId },
-        data: { inventory: { set: washer.inventory.filter((i) => i !== itemId) } },
-    });
+  await prisma.item.update({
+    where: { id: itemId },
+    data: { owner: vendorId },
+  });
 
-    await prisma.item.update({
-        where: { id: itemId },
-        data: { owner: vendorId },
-    });
-
-    return `Item(${item.id}) was moved from Washer(${washerId}) to Vendor(${vendorId})`;
+  return `Item(${item.id}) was moved from Washer(${washerId}) to Vendor(${vendorId})`;
 }
 
-module.exports = {moveToVendor};
+module.exports = { moveToVendor };
