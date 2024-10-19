b9449020e14bb626e0fdfda7cdc7942c5b105a8e
danteb98 <danteb623@gmail.com>
2024-10-18 21:11:42 -0400

    base functionality for user registration, login and authentication
    base functionality

--- server/api/server.js
+++ server/api/server.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const usersRoutes = require('./routes/users.js');
-const vendorsRoutes = require('./routes/vendors.js');
-const washerRoutes = require('./routes/washer.js');
-const stationsRoutes = require('./routes/stations.js');
-const itemsRoutes = require('./routes/items.js');
-const discountsRoutes = require('./routes/discounts.js');
-const authRoutes = require('./routes/auth.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/users.js");
+const vendorsRoutes = require("./routes/vendors.js");
+const washerRoutes = require("./routes/washer.js");
+const stationsRoutes = require("./routes/stations.js");
+const itemsRoutes = require("./routes/items.js");
+const discountsRoutes = require("./routes/discounts.js");
+const authRoutes = require("./routes/auth.js");
 
 const app = express();
-app.use(bodyParser.json());
 const PORT = 5000;
 
-app.use('/api/v1/users', usersRoutes);
-app.use('/api/v1/vendors', vendorsRoutes);
-app.use('/api/v1/washer', washerRoutes);
-app.use('/api/v1/stations', stationsRoutes);
-app.use('/api/v1/items', itemsRoutes);
-app.use('/api/v1/discounts', discountsRoutes);
-app.use('/api/v1/auth', authRoutes);
+app.use(bodyParser.json());
+
+//CAN USE THIS TO TEST IF POSTMAN API CALLS GOING THROUGH TO VS-CONSOLE.
+app.use((req, res, next) => {
+  console.log(`Received ${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.use("/api/v1/users", usersRoutes);
+app.use("/api/v1/vendors", vendorsRoutes);
+app.use("/api/v1/washer", washerRoutes);
+app.use("/api/v1/stations", stationsRoutes);
+app.use("/api/v1/items", itemsRoutes);
+app.use("/api/v1/discounts", discountsRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
